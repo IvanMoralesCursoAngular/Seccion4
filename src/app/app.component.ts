066081aadf83e80162e296cc057489037f17c228
Ivan Morales Angular <ivan.morales.curso.angular@hotmail.com>
2025-01-19 23:23:30 -0700

    funcionalidad del contador( se encapsulo en un componente )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public title: string = 'Mi primera app de Angular';
-  public counter: number = 10;
+  public title: string = 'Desde appModule';
 
-  //la funcion tiene un public por default, es mas comun no ponerselo
-  increseBy( value:number ):void {
-    this.counter += value;
-  }
-
-  reset() {
-    this.counter = 10;
-  }
 
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  templateUrl: './counter.component.html'
+  template: `<h3>Counter: {{counter}}</h3>
+
+          <button (click)="increseBy(+1)">+1</button>
+          <button (click)="reset()">reset</button>
+          <button (click)="increseBy(-1)">-1</button>
+          `
 })
 export class CounterComponent {
+
+  public counter: number = 10;
+
+  //la funcion tiene un public por default, es mas comun no ponerselo
+  increseBy( value:number ):void {
+    this.counter += value;
+  }
+
+  reset() {
+    this.counter = 10;
+  }
 
 }
