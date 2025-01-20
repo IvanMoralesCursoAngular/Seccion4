75ffe81309372be8e38da8adaf42a4a063f30056
Ivan Morales Angular <ivan.morales.curso.angular@hotmail.com>
2025-01-20 15:33:45 -0700

    Onew way data binding - enlazado en una sola vista

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -10,4 +10,15 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age:  number = 45;
 
+  //Se miran como si fueran propiedades, pero no lo son. Al llamarse en el html se llama sin parentesis
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  //No es un get, y al llamarse en el html si se llama con el parentesis
+  //Al ponerse como private solo estara disponible en el scope de esta clase, no fuera.
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
 }
